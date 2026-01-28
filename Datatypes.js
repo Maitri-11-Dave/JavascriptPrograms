cf97977b73a8ba1953dd2426e10cf8c27a354e14
Maitri dave <maitriddave2511@gmail.com>
2026-01-28 13:21:26 +0530

    [MD] [varible & Datatypes filesare added]

---
+++ Datatypes.js
@@ -0,0 +1,161 @@
+//two types of data type 
+    //1.primitive
+    //Number 
+    //BigInt
+    //String
+    //Boolean
+    //Symbol
+    //null
+    //undefined
+
+
+//2.Non-Primitive
+    //Array
+    //Object
+    //Finction
+
+//NUMBER
+    // let num = 1;
+    // let num2 = 2;
+
+    // console.log("num",num,num2);
+
+//String
+    // let name = "Maitri";
+    // let name2 = "Krishna";
+    // console.log ("names :", name2,name)
+
+//Boolean 
+    //let isTrue = true;
+   // let isFlase = false;
+    //console.log("Boolean :", isTrue)
+
+//undefine
+    //let a;
+    //console.log("indefined :", a)
+
+//BIGINT
+    //let BigInt = 123456678901n;
+    //console.log(BigInt)
+
+
+//2 . Non-Primitive
+
+//Object
+    //let student = {
+      //  name : "Maitri",
+        //roll : 33,
+       // address :{
+       //     city:"Jnd",
+  //      }
+   // }
+   // console.log(student);
+
+//Array
+    //let a = [1,2,3,4,5,6,7,8,9]
+    //console.log(a);
+
+//Function
+    // function greet(){
+    //     console.log("This is Function");
+    // }
+    // greet();
+
+    // function ShowValues(value){
+    //     console.log("this is value :",value);
+    //     if(value == 20){
+    //         console.log("number-----")
+    //     }
+    // }
+    // ShowValues();
+    //     ShowValues(20);
+    //             ShowValues([1,2,3,4,5]);
+
+
+
+
+    //**Example**//
+
+
+    //**Addition */
+//     function add (num1,num2){
+//     console.log("add is ",num1+num2);
+// }
+// add(12,12);
+
+//**Substraction**/
+//  function sub (num1,num2){
+//     console.log("sub is ",num1-num2);
+// }
+// sub(20,12);
+
+//Multiplication
+// function multi(num1,num2){
+//     console.log("multi is ",num1*num2);
+// }
+// multi(2,12);
+
+//*Division */
+// function div (num1,num2){
+//     console.log("div is ",num1/num2);
+// }
+// div(12,2);
+
+// const add = (num1,num2)=>{
+//     return num1+num2
+// }
+// console.log("this is arrow function", add(5,10))
+
+// const sub = (num1,num2)=>{
+//     return num1-num2
+// }
+// console.log("this is arrow function", sub(10,5))
+
+// const div = (num1,num2)=>{
+//     return num1/num2
+// }
+// console.log("this is arrow function", div(5,10))
+
+// const multi = (num1,num2)=>{
+//     return num1/num2
+// }
+// console.log("this is arrow function", multi(12,3))
+
+// let value = 10;
+// console.log('DEFAULT VALUES :',value);
+
+// value += 5 // value + 5
+// console.log(value)
+
+// value -= 5 // value - 5
+// console.log(value)
+
+// value *= 5 // value * 5
+// console.log(value)
+
+// value /= 5 // value / 5
+// console.log(value)
+
+// let age =20;
+// let hasId = true
+// let name = "maitri"
+// let object = {
+//     name:"exmp",
+//     roll:20
+// }
+
+// console.log("logical AND (&&) : ", age > 18 && hasId)
+// console.log("logical OR (||) : ", age < 18 || hasId)
+// console.log("logical NOT (!) : ",  ! hasId)
+
+// console.log(typeof age)
+// console.log(typeof hasId)
+// console.log(typeof name)
+// console.log(typeof object)
+
+let value = 10;
+let num= "10"
+
+console.log("value == num : ",value == num);
+console.log("value === num : ",value === num);
+console.log("value != num : ",value != num);
---
+++ variable.js
@@ -0,0 +1,16 @@
+//VAR
+var a = 10;
+console.log(10); 
+
+//LET
+let a = 20;
+
+if(true){
+    let a = 10;
+}
+console.log(a);
+
+//CONST
+
+const pi = 13.2;
+console.log(a);
